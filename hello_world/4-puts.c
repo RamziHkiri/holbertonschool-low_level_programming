b544ece9c3f31bef8236e9885d8900ed64abdcbc
RamziHkiri <6257@holbertonstudents.com>
2023-02-19 23:22:16 -0800

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+/*
+ *Write a C iprogram that prints exactly "Programming is like building a multilingual puzzle", followed by a new line.
+
+    Use the function puts
+    You are not allowed to use printf
+    Your program should end with the value 0
+
+ *
+ */
 int main(void)
 {
+	/*
+	 *print message using puts
+	 *you ar not allowed to use printf
+	 * /
+	
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
