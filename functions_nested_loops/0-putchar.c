f2080099de0f840f991465b8158bfca7d60b666b
RamziHkiri <6257@holbertonstudents.com>
2023-02-21 07:36:00 -0800

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<main.h>
+#include"main.h"
 /**
  * main - check the code
  *
@@ -7,7 +7,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
