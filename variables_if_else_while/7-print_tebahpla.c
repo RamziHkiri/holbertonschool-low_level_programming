dbe59a6291d81abbac617b10fec60af590ae263e
RamziHkiri <6257@holbertonstudents.com>
2023-02-21 01:01:22 -0800

    smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s;
+
+	for (s = 'z' ; s >= 'a'; s--)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
