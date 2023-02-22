fa3d30aee98d359dde2c69bfb0e90a3d2603d68d
RamziHkiri <6257@holbertonstudents.com>
2023-02-22 03:44:45 -0800

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * print_last_digit - test if a character is lower case or no
- * @n: integer used 
+ * @n: integer used
  *
  * Return: return last number of n
  */
 int print_last_digit(int n)
 {
 	int r;
-	r =(n % 10);
+
+	r = (n % 10);
 	if (r > 0)
 	{
 		_putchar(r + '0');
