3aa267b6d566c9ee0324abda858b7eda42b3573f
RamziHkiri <6257@holbertonstudents.com>
2023-02-22 01:16:40 -0800

    c is lower or no

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _islower - test if a character is lower case or no
+ * @c: the tested character
  *
- * Return: return 1 for lowercase character and 0 for others
+ * Return: 1 for lowercase character and 0 for others characters
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <=122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
