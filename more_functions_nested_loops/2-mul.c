aac8a469bf8393ae85fd09ebd6445d65c882a1a4
RamziHkiri <6257@holbertonstudents.com>
2023-02-24 08:22:51 -0800

    multiplicationx

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * mul - test if a char c is upper case or no
+ *@a: integer
+ *@b: integer
+ * Return: 1  if c is upper 0 if not
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
