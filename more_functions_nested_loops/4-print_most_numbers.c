10ba549e9f65d9e4cd1811d7372dd6dff491f451
RamziHkiri <6257@holbertonstudents.com>
2023-02-24 08:44:20 -0800

    print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_most_numbers - print numbers from 1 to 9 followed by new line
+ *
+ * Return: return 0123456789
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i != 2 && i != 4)
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
