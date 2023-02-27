9710454a54f5bda2cdfdf717ab1669773a7e1ca1
RamziHkiri <6257@holbertonstudents.com>
2023-02-27 01:39:11 -0800

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_square - print numbers from 1 to 9 followed by new line
+ * @size: size of square
+ * Return: return 0123456789
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0 ; j < size ; j++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
