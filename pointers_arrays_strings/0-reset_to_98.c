f24532f2c111a904186ddcc712c94c8a24fe99a6
RamziHkiri <6257@holbertonstudents.com>
2023-02-27 07:30:52 -0800

    reset to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include"main.h"
 /**
- * et_to_98 - apdate the value of a pointer to 98
+ * reset_to_98 - apdate the value of a pointer to 98
  *
  * Return: return 0123456789
  */
 void reset_to_98(int *n)
 {
-	*n=98;	
+	*n = 98;
 }
