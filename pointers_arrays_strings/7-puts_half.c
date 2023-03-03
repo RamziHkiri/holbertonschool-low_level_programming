fb06d0d4bc50747a17d79c16a71a8ab9b1cae28b
RamziHkiri <6257@holbertonstudents.com>
2023-03-03 08:27:58 -0800

    puts half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,9 @@ void puts_half(char *str)
 
 	for (n = 0 ; str[n] != '\0' ; n++)
 		;
-	for (i = (n - 1) / 2 ; i < n ; i++)
+	for (i = (n - 1) / 2 + 1 ; i < n ; i++)
 	{
-		_putchar(str[i + 1]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
