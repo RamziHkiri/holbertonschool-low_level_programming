10f0c08c09db41613aa977403c01ff7dce1ddadf
RamziHkiri <6257@holbertonstudents.com>
2023-03-05 09:45:47 -0800

    coppy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,9 +9,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
+
 	for (j = 0 ; src[j] != '\0' ; j++)
 		;
-	for (i = 0 ; i < n &&  i <= j ; i++)
+	for (i = 0 ; i < n  &&  i <= j ; i++)
 	{
 		dest[i] = src[i];
 	}
