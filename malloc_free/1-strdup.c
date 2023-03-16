6d955df4a38fff3aeaaa43d66eea74720294dc3d
RamziHkiri <6257@holbertonstudents.com>
2023-03-16 05:20:36 -0700

    string duplication

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include"main.h"
+#include <stdlib.h>
+/**
+ * _strdup -duplicate a string using malloc
+ * @str:duplicated string
+ * Return:res
+ */
+char *_strdup(char *str)
+{
+	char *res;
+	int i, j;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; str[i] != '\0'; i++)
+		;
+	res = malloc(sizeof(char) * i);
+	if (!res)
+	{
+		return (NULL);
+	}
+	for (j = 0 ; str[j] != '\0' ; j++)
+	{
+		res[j] = str[j];
+	}
+	res[j] = '\0';
+	return (res);
+}
