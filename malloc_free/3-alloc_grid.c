e6bce059b6dc3ed0f519b27798ea95985891f2b9
RamziHkiri <6257@holbertonstudents.com>
2023-03-17 10:02:47 -0700

    allocation memoire

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+/**
+ * alloc_grid - allocated a matrice
+ * @width:integer
+ * @height:integer
+ * Return:grid or NULL if the allocation failed
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (!grid)
+		return (NULL);
+	for (i = 0 ; i < height ; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+	}
+	for (i = 0 ; i < height ; i++)
+	{
+		for (j = 0 ; j < width ; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
