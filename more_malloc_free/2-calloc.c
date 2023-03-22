263fd3f7fdf8b2a3a4b79927be25987c8f5178ba
RamziHkiri <6257@holbertonstudents.com>
2023-03-22 00:55:35 -0700

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,7 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *alloc;
+	char *alloc;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,6 +18,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (!alloc)
 		return (NULL);
-
+	for (i = 0 ; i < size * nmemb ; i++)
+	{
+		alloc[i] = '\0';
+	}
 	return (alloc);
 }
