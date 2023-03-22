6875f427e8019e132969112215c11a57296f46ad
RamziHkiri <6257@holbertonstudents.com>
2023-03-22 01:53:02 -0700

    array range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(int) * (max - min) + 2);
+	arr = malloc(sizeof(int) * (max - min) + 3);
 	if (!arr)
 	{
 		return (NULL);
