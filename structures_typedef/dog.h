f80af26b8012fe92814f443bc38bb0485b1b7e2b
RamziHkiri <6257@holbertonstudents.com>
2023-03-24 08:54:38 -0700

    dog tructuretype

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - data structure for dog
+ * @name:the name of the dog
+ * @age:the age of the dog
+ * @owner:the owner ofthe dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif²
