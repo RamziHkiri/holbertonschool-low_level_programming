ac31a51935da8bf730c0bcbada11232189899893
RamziHkiri <6257@holbertonstudents.com>
2023-03-27 02:15:45 -0700

    test

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,10 +13,18 @@ int main(int argc, char *argv[])
 	char *op;
 
 	if (argc != 4)
-		return(1);
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	op = argv[2];
+	if ((op == '%' && num2 == 0) || (op == '/' && num2 = 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	res = get_op_func(op)(a,b);
 	printf("%d\n",res);
 	return(0);
