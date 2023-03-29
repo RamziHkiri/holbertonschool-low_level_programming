86cdc3e4898a068f80fdf6d5a128f2db72e012f1
RamziHkiri <6257@holbertonstudents.com>
2023-03-29 13:08:15 -0700

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,13 @@
+#include<stdarg.h>
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+typedef struct print
+{
+	char *c;
+	void (*print_func)(va_list params);
+}print_t;
 #endif
