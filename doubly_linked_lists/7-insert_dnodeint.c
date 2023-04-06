9cdf4f5d690f3ad8d460f8f715bbb0c3b6422263
RamziHkiri <6257@holbertonstudents.com>
2023-04-06 04:21:37 -0700

    insert node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -30,6 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			nlist->prev->next = node;
 			return (nlist);
 			}
+			i++;
 			nlist = nlist->next;
 		}
 	return (NULL);
