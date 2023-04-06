4de0fc72fb3322c3e9a1ea768963f767132e87c6
RamziHkiri <6257@holbertonstudents.com>
2023-04-06 01:38:37 -0700

    get node by index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - add a node into beginning of a doubly linked list
+ * @head:pointer to the linked list
+ * @index:the index of the searched node
+ * Return: node of the list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node = head;
+	unsigned int i = 0;
+
+	while (node && i <= index)
+	{
+		node = node->next;
+		i++;
+		if (i == index)
+			return (node);
+	}
+	return (NULL);
+}
