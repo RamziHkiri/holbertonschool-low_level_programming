990c988db25ef18b6ba16d65240f6c8c03799219
RamziHkiri <6257@holbertonstudents.com>
2023-04-06 03:11:11 -0700

    insert node into doubly linked list

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "lists.h"
+/**
+ * dlistint_t *insert_dnodeint_at_index - insert a node at a given index
+ * @h:linked list
+ * @idx:the index where to insert our integer
+ * @n:integer to be inserted
+ * Return:pointer to the new doubly linked list
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *node, *nlist;
+	unsigned int i = 0;
+		node = malloc(sizeof(dlistint_t));
+		if (!node)
+			return (NULL);
+		node->n = n;
+		nlist = *h;
+		while (nlist && i <= idx)
+		{
+			if (i == idx)
+			{
+			node->prev = nlist->prev;
+			node->next = nlist;
+			nlist->prev = node;
+			nlist->prev->next = node;
+			return (nlist);
+			}
+			nlist = nlist->next;
+		}
+	return (NULL);
+}
+
