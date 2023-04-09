424bfdc7ee9c4e5bbf717ec971a37904363d3842
RamziHkiri <6257@holbertonstudents.com>
2023-04-09 09:57:36 -0700

    convert from int to binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_binary - convert integer to binary
+ * @n:integer
+ * Return:none
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
