932b6c223d07bba29af8b112c4eb2df215ebad59
RamziHkiri <6257@holbertonstudents.com>
2023-04-10 00:14:00 -0700

    get bit by index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * get_bit - get the bit at the index given
+ * @n:long int
+ * @index:the index to return
+ * Return:the bite to find
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	n = n >> index;
+	return (1 & n);
+}
