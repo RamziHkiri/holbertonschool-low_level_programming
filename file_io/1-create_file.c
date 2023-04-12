43f96bf46090dbdeb371a1e55d3e813ac3b25971
RamziHkiri <6257@holbertonstudents.com>
2023-04-12 05:22:05 -0700

    create new file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, o, w;
+	int i, o;
 
 if (!filename)
 	return (-1);
@@ -26,7 +26,7 @@ if (text_content != NULL)
 		;
 }
 o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-w = write(o, text_content, i);
+write(o, text_content, i);
 if (o < 0)
 {
 	close(o);
