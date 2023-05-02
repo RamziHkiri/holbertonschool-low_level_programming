7179658e9e85492b30a4b7286dc07167f827734e
RamziHkiri <6257@holbertonstudents.com>
2023-05-02 10:45:00 -0700

    appendtext to file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,33 +1,30 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include"main.h"
-#include<stddef.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
+#include "main.h"
 /**
- *  append_text_to_file - append text to the end of the file
- *  @filename: pointer to the file name
- *  @text_content: string to add to the file
- *  Return:if function fails -1 else return 1
+ * append_text_to_file - function that appands text at the end of a file
+ * @filename: file name
+ * @text_content: text
+ * Return: 1 on success -1 on faillure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i, w, o;
+	int o, w, len = 0;
 
-	if (!filename || !text_content)
+		if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
-	{
-		for (i = 0 ; text_content[i] ; i++)
-			;
-	}
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, i);
-	if (o < 0 || w < 0)
+
+		if (text_content != NULL)
+			len = strlen(text_content);
+
+		o = open(filename, O_WRONLY | O_APPEND);
+		w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+
 		return (-1);
+
 	close(o);
 	return (1);
+
+
+
 }
